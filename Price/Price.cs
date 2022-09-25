0eeca55e1e84bb2eaecbb64c94394d87eb957031
vova.chakalov.2003 <vova.chakalov.2003@gmail.com>
2022-09-25 16:38:08 +0300

    Приветули. Магем добавлять товары

---
+++ Price/Price.cs
@@ -0,0 +1,21 @@
+﻿namespace Price
+{
+    public struct Price
+    {
+        public string Name;
+        public string Shop;
+        public int Cost;
+
+        public Price(string name, string shop, int cost)
+        {
+            Name = name;
+            Shop = shop;
+            Cost = cost;
+        }
+
+        public string ToString()
+        {
+            return $"{Name} {Shop} {Cost}";
+        }
+    }
+}
--- Price/Program.cs
+++ Price/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,23 @@ namespace Price
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите желаемое количество товаров");
+            int n = int.Parse(Console.ReadLine());
+            Price[] arrayOfPrices=new Price[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Товар номер {i+1}");
+                Console.Write("Введите название товара:");
+                string name = Console.ReadLine();
+                Console.Write("Введите название магазина:");
+                string shop= Console.ReadLine();
+                Console.Write("Введите цену товара:");
+                int cost = int.Parse(Console.ReadLine());
+
+                Price price = new Price(name, shop, cost);
+                arrayOfPrices[i] = price;
+            }
+
         }
     }
 }
